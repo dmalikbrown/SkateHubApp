aa3d57232f36e9cde27d7dd8b5e51f1dbb3c3764
woodi3 <wawood@email.sc.edu>
2017-10-29 19:53:52 -0400

    Added basic necessary packages

--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
 //This is our server file
 //Making my first Commit - TJ.
 // console.log("Hello WOrld");
+
+const express = require('express');
+//const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//const passport = require('passport');
+const mongoose = require('mongoose');
