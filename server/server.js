15edd67b9efff836da4c1d4942217caff7044163
woodi3 <wawood@email.sc.edu>
2017-10-29 20:02:10 -0400

    Connected to mlab db

--- server/server.js
+++ server/server.js
@@ -8,3 +8,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 //const passport = require('passport');
 const mongoose = require('mongoose');
+
+// Connect To Database
+var dbUrl = "mongodb://wawood:password@ds241025.mlab.com:41025/skatehub";
+mongoose.Promise = require('bluebird');
+var options = {
+  useMongoClient: true,
+  socketTimeoutMS: 0,
+  keepAlive: true,
+  reconnectTries: 30
+};
+mongoose.connect(dbUrl, options);
+
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+dbUrl);
+});
