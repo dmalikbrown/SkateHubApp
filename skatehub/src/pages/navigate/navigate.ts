0f437789c267b8ce97751b74e557e0f7edac8ef0
pbateman828 <pbateman828@gmail.com>
2017-12-02 23:37:38 -0500

    Error fixed, comments added

--- skatehub/src/pages/navigate/navigate.ts
+++ skatehub/src/pages/navigate/navigate.ts
@@ -44,7 +44,88 @@ export class NavigatePage {
     google.maps.visualRefresh = true;
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 42.360091, lng: -71.0963487},
-      zoom: 20
+      zoom: 15,
+      // there are different map styles, this is night mode
+      styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
     });
     let infoWindow = new google.maps.InfoWindow;
 
@@ -56,15 +137,37 @@ export class NavigatePage {
             lng: position.coords.longitude
           }
 
+          var marker = new google.maps.Marker({
+            position: pos,
+            map: this.map,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            title: 'Hello World!'
+          })
+
+          //sets position of infoWindow
           infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found.');
-          infoWindow.open(this.map);
+
+          //when the marker is clicked, open infoWindow
+          marker.addListener('click', () => {
+            infoWindow.open(this.map, marker);
+          })
+
+          //JSON stringify to print pos object in infoWindow
+          infoWindow.setContent(JSON.stringify(pos));
+          console.log(pos);
+
+          // infoWindow.open(this.map);
+
           this.map.setCenter(pos);
+
+          //HTML5 geolocation checking
         }, () => {
           this.handleLocationError(true, infoWindow, this.map.getCenter());
 
         });
     } else {
+      // browser doesn't support geolocation
       this.handleLocationError(false, infoWindow, this.map.getCenter());
     }
 
