c23adf9ae1910986e5b0e88f06ecfc2425fccac8
woodi3 <wawood@email.sc.edu>
2017-12-03 23:43:56 -0500

    added search functionality to search page

--- skatehub/src/pages/search/search.ts
+++ skatehub/src/pages/search/search.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
+import { SpotsProvider } from '../../providers/spots/spots';
 
 /**
  * Generated class for the SearchPage page.
@@ -16,12 +17,20 @@ import { AuthProvider } from '../../providers/auth/auth';
 })
 export class SearchPage {
 
+  spots: any = [];
+  users: any = [];
+  user: any;
+  searchTerm: any;
+  userResultArr: any = [];
+  spotsResultArr: any = [];
+  start: boolean = true;
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public authProvider: AuthProvider) {
+              public authProvider: AuthProvider, public spotsProvider: SpotsProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
+    // console.log('ionViewDidLoad SearchPage');
   }
 
   ionViewCanEnter(){
@@ -33,5 +42,61 @@ export class SearchPage {
          return false;
      });
   }
+  ionViewDidEnter(){
+    if(this.authProvider.user){
+      this.user = this.authProvider.user;
+      // console.log(this.user);
+    }
+    else{
+      this.user = this.navParams.data;
+      // console.log(this.user);
+    }
+    this.getUsers();
+    this.getSpots();
+  }
+  getSpots(){
+    this.spotsProvider.getAllSpots().subscribe((data) => {
+      if(data.success){
+        this.spots = data.spots;
+      }
+      else{
+        //TODO error check;
+      }
+    });
+  }
+  getUsers(){
+    this.authProvider.getAllUsers().subscribe((data)=>{
+      if(data.success){
+        this.users = data.users;
+      }
+      else {
+        //TODO error check;
+      }
+    });
+  }
+  filter(){
+    this.filterUsers();
+    this.filterSpots();
+  }
+  filterUsers() {
+    this.start = false;
+    if(this.searchTerm && this.searchTerm.trim() != ''){
+      this.userResultArr = this.users.filter(user => user.username.includes(this.searchTerm.toLowerCase()) || user.fullName.toLowerCase().includes(this.searchTerm.toLowerCase()));
+    }
+    else{
+      this.userResultArr = [];
+    }
+ }
+ filterSpots(){
+
+   this.start = false;
+   if(this.searchTerm && this.searchTerm.trim() != ''){
+     this.spotsResultArr = this.spots.filter(spot => spot.name.toLowerCase().includes(this.searchTerm.toLowerCase()));
+   }
+   else{
+     this.spotsResultArr = [];
+   }
+ }
+
 
 }
