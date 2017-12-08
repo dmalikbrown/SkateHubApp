800f266b13eeae5c3761788d7810a09307d165c0
woodi3 <wawood@email.sc.edu>
2017-12-07 20:04:59 -0500

    Added heroku url

--- skatehub/src/pages/post/post.ts
+++ skatehub/src/pages/post/post.ts
@@ -38,6 +38,7 @@ export class PostPage {
   resultArr: any = []; //typically going to be an array of strings
   skateTypes: any = []; //typically going to be an array of strings
   devEp = "http://localhost:3000"; //end point for the server when in dev mode
+  prodEp = "https://skatehub.herokuapp.com/";
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public authProvider: AuthProvider, public spotsProvider: SpotsProvider,
@@ -257,7 +258,7 @@ export class PostPage {
       const fileTransfer: FileTransferObject = this.transfer.create();
 
       //send the file to the routes in the router.js file
-      fileTransfer.upload(this.imageNewPath, encodeURI(this.devEp+'/skatehub/image/upload'),
+      fileTransfer.upload(this.imageNewPath, encodeURI(this.prodEp+'/skatehub/image/upload'),
         options).then((entry) => {
           //entry is returned from the server.
           console.log(entry);
